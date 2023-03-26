d482438fdbc09541995944c9746f495eac986c95
Dmitriy Tarasov <tarasovdtk@gmail.com>
2023-03-26 10:54:12 +0400

    Added new Features

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({increased, employees}) => {
     return (
         <div className="app-info">
             <h1>Employee Accounting in Comapny N</h1>
-            <h2>Total number of employees: </h2>
-            <h2>Will receive the award: </h2>
+            <h2>Total number of employees: {employees}</h2>
+            <h2>Will receive the award: {increased}</h2>
         </div>
     )
 }
